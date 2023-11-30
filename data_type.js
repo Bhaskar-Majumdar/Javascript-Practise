b543ca9c9bf4e2b55ed2cc6c7021340554eecddf
Bhaskar Majumdar <66664289+Bhaskar-Majumdar@users.noreply.github.com>
2023-11-30 16:37:18 +0600

    Add files via upload

---
+++ data_type.js
@@ -0,0 +1,16 @@
+let a = 55;
+let b = 100.55;
+let c = "Bhaskar";
+let d = 'Majumdar';
+let e = true;
+let U 
+let f = null;
+
+console.log (typeof a)
+console.log (typeof b)
+console.log (typeof c)
+console.log (typeof d)
+console.log (typeof e)
+console.log (typeof u)
+console.log (typeof f)
+
