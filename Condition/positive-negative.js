21c2a8273d74a63dd0f804878b0185fc14506a5a
Bhaskar Majumdar <66664289+Bhaskar-Majumdar@users.noreply.github.com>
2024-07-28 00:35:44 +0600

    condition practise

---
+++ Condition/positive-negative.js
@@ -0,0 +1,17 @@
+//Checks if the number is positive/negative/zero
+const prompt = require("prompt-sync")();
+const number = parseInt(prompt("Enter a number: "));
+
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("The number is negative");
+}
+
+
