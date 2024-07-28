59d9c153742cba9a5e6c5fa1acd4948e549ac8c8
Bhaskar Majumdar <66664289+Bhaskar-Majumdar@users.noreply.github.com>
2024-07-28 23:14:49 +0600

    for loop practise

---
+++ Loop/for_loop_practise1.js
@@ -0,0 +1,17 @@
+var numbers = [1,2,3,4,5,6,7,"Abul","Kalam","Azad"];
+
+for (var i = 0; i < numbers.length; i++){
+    console.log (numbers[i]);
+}
+console.log (i);
+
+
+
+//////////(1-5)numbers sumation print/////////////
+let sum = 0;
+for (let i = 1; i <= 5; i++){
+    console.log ('sum =',sum, 'i =',i,'sum+i =',sum+i);
+    sum = sum + i;
+}
+console.log ("sum =",sum); 
+
